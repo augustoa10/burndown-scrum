5300331f664c651703e431b6b3f93988fafbe9d1
Danilo Siqueira <dan.csiqueira@gmail.com>
2016-04-14 10:49:45 -0300

    Database adapter

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,6 +1,6 @@
 class SprintsController < ApplicationController
   def index
     @sprint = Sprint.last
-    @goals = { name: "Sprint Total Points #{ rand(100) }", data: @sprint.goals.count }
+    @goals = { name: "Sprint Total Points #{ rand(100) }", data: @sprint.goals.group_by_week(:created_at, week_start: :mon).count }
   end
 end
